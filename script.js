8c2a64193a4a9d1b8a13748e4dfc45572b5d8b9e
cigna <whitebownitro@gmail.com>
2025-05-18 14:44:34 -0400

    fix, code funcional

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 
-const estudiantes=[] //Arreglo, almacena alumno ingresado (nombre, apellido,nota)
+const estudiantes = [] //Arreglo, almacena alumno ingresado (nombre, apellido,nota)
 
-const tablaBody = document.querySelector("studenTable tbody");
+const tablaBody = document.querySelector("#studentsTable tbody");
 // Inicialmente tbody = empty
 // Cuando se agrega un estudiante, JS crea una fila y la inserta dentro del tbody, usando appendChild.
 
@@ -12,31 +12,20 @@ const promedioDiv = document.getElementById("average") //Selecicona el div donde
 document.getElementById("studentForm").addEventListener("submit", function (e) {
     e.preventDefault(); //(no recarga la página al apretar el boton).
 
-    const name=document.getElementById("name").value.trim(); //obtiene el nombre del estudiante (sin espacios a los lados).
-    const lastName=document.getElementById("lastName").value.trim();
-    const grade=parseFloat(document.getElementById("grade").value); //nota se convierte en numero decimal
+    const name = document.getElementById("name").value.trim(); //obtiene el nombre del estudiante (sin espacios a los lados).
+    const lastName = document.getElementById("lastName").value.trim();
+    const grade = parseFloat(document.getElementById("grade").value); //nota se convierte en numero decimal
 
     //validacion de datos
-    if(!name){
-        alert("Por favor, complete el campo 'Nombre'")
-        return
-    }
-    if(!lastName){
-        alert("Por favor, complete el campo 'Apellido'")
-        return
-    }
-    if(isNaN(grade)){
-        alert("Por favor, ingrese un numero valido")
-        return
-    }
-    if(grade<1 || grade>7){
-        alert("Debe estar en rango valido 1.0 y 7.0")
+
+    if(!name || !lastName || isNaN(grade) || grade<1 || grade>7){
+        alert("Error al ingresar Datos")
         return
     }
 
 
     //guarda el estudiante
-    const estudiante={name,lastName,grade};
+    const estudiante = { name, lastName, grade };
     //Lo agrega al arreglo estudiantes
     estudiantes.push(estudiante);
     addStudentToTable(estudiante) //anade visualmente a la tabla.
@@ -50,21 +39,20 @@ document.getElementById("studentForm").addEventListener("submit", function (e) {
 function addStudentToTable(estudiante) {
     const row = document.createElement("tr");
     row.innerHTML = //innerHTML para insertar tres columnas: nombre, apellido, nota.
-    `
-        <td>${estudiante.name}</td>
-        <td>${estudiante.lastName}</td>
-        <td>${estudiante.grade}</td>
-    `;
-    tableBody.appendChild(row); //Añade esa fila al cuerpo de la tabla studentsTable.
+        `
+    <td>${estudiante.name}</td>
+    <td>${estudiante.lastName}</td>
+    <td>${estudiante.grade}</td>`;
+    tablaBody.appendChild(row); //Añade esa fila al cuerpo de la tabla studentsTable.
 }
 
 function calcularPromedio() {
     if (estudiantes.length === 0) {
-        averageDiv.textContent = "Promedio General del Curso : N/A"; //Si no hay estudiantes, muestra n/a.
+        promedioDiv.textContent = "Promedio General del Curso : N/A"; //Si no hay estudiantes, muestra n/a.
         return;
     }
     const total = estudiantes.reduce((sum, estudiante) => sum + estudiante.grade, 0); //sumar todas las nota
     const prom = total / estudiantes.length;
-    averageDiv.textContent = "Promedio General del Curso : " + prom.toFixed(2);
+    promedioDiv.textContent = "Promedio General del Curso : " + prom.toFixed(2);
     //Muestra el resultado con 2 decimales usando .toFixed(2).
 }
